4dfd059abf3b3ec0f79cd5c536b37f5c8cb1bbb3
Sameer Jain <72691195+Sameer4522@users.noreply.github.com>
2022-09-21 10:09:34 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let operators = ["+", "-", "*"];
+let answerValue;
+let operatorQuestion;
+const startBtn = document.getElementById("start-btn");
+const question = document.getElementById("question");
+const controls = document.querySelector(".controls-container");
+const result = document.getElementById("result");
+const submitBtn = document.getElementById("submit-btn");
+const errorMessage = document.getElementById("error-msg");
+
+// Random Value Generator
+const randomValue = (min, max) =>;
