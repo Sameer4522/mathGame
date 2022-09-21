1b748e81685f0d60916592e30b930a61c7e4b3fb
Sameer Jain <72691195+Sameer4522@users.noreply.github.com>
2022-09-21 11:38:11 +0530

    third commit

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ const submitBtn = document.getElementById("submit-btn");
 const errorMessage = document.getElementById("error-msg");
 
 // Random Value Generator
-const randomValue = (min, max) =>;
+const randomValue = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+// Question Generator
+const questionGenerator = () => {
+  // two random values between 1 to 20
+  let [num1, num2] = [randomValue(1, 20), randomValue(1, 20)];
+
+  // Random Operator
+  let randomOperator = operators[Math.floor(Math.random() * operators.length)];
+
+  if (randomOperator == "-" && num2 > num1) {
+    [num1, num2] = [num2, num1];
+  }
+};
